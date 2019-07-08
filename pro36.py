d0f00dd4b07dccf85817ac9e61395441f2b87ae2
cartosat <cartosat4@gmail.com>
2019-07-08 23:59:35 +0530

    My pratice
    Signed-off-by: cartosat <cartosat4@gmail.com>

--- pro36.py
+++ pro36.py
@@ -3,10 +3,18 @@
 class car:
 
     wheels=4
+    com="BMW"
+    mil=12
 
     def __init__(self):
         self.com="BMW"
         self.mil=12
+
+    def faci(self):
+
+        print("car has audio tape")
+
+
 
 
 c1=car()
@@ -14,5 +22,5 @@ c2=car()
 
 car.wheels=5
 
-print(c1.com,c1.mil,c1.wheels)
+print(c1.com,c1.mil,c1.wheels,c1.faci())
 print(c2.com,c2.mil,car.wheels)
